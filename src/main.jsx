13f749652906f6fe4b55ba5cd6bbe167a33165fc
ManiKandan2129 <gokulvirat2129@gmail.com>
2023-08-30 18:10:59 +0530

    price card

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Price from './Price.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  
+    <Price></Price>
+
+  </React.StrictMode>,
+)
